6765bcbb30d495902a2ddbdfcb569deb7ca108b6
inxeoz <pk@pk.com>
2024-12-21 23:33:10 +0530

    hashset is power

--- src/feature.rs
+++ src/feature.rs
@@ -1,37 +1,39 @@
-
+use serde_json::{json, Map, Value};
 use sha1::{Digest, Sha1};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fmt::format;
 use std::fs::{File, OpenOptions};
 use std::io::{self, prelude::*};
-use std::path::{Path, PathBuf};
-use std::{env, fs};
-use std::collections::{HashMap, VecDeque};
-use std::fmt::format;
-use serde_json::{json, Map, Value};
 use std::io::{Read, Write};
-
+use std::path::{Path, PathBuf};
+use std::{env, fs, path};
 
 pub fn write_to_file(file_path: &str, content: &str) {
     let path = Path::new(file_path);
 
     if let Some(parent) = path.parent() {
-        fs::create_dir_all(parent).unwrap_or_else(|_| eprintln!("Failed to create folder '{}'.", parent.display()));
+        fs::create_dir_all(parent)
+            .unwrap_or_else(|_| eprintln!("Failed to create folder '{}'.", parent.display()));
     }
 
-    let mut file = OpenOptions::new().write(true).create(true).append(true).open(path)
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(path)
         .unwrap_or_else(|_| panic!("Failed to open file '{}'.", file_path));
 
     write!(file, "{}", content).expect("Failed to write on file");
 }
 
-pub fn print_current_path() {
-    env::current_dir().map_or_else(
-        |e| eprintln!("Error getting current path: {}", e),
-        |path| println!("Current path: {}", path.display()),
-    );
+pub fn get_current_path() -> PathBuf {
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    // current_dir.join("version_control_system")
+    current_dir
 }
 
 pub fn create_folder_if_not_exists(folder_path_from_root: &str) {
-    let folder_path = env::current_dir().expect("Error getting current directory").join(folder_path_from_root);
+    let folder_path = get_current_path().join(folder_path_from_root);
     if !folder_path.exists() {
         fs::create_dir_all(&folder_path).expect("Cannot create folder.");
         println!("Folder '{}' created successfully.", folder_path.display());
@@ -39,7 +41,7 @@ pub fn create_folder_if_not_exists(folder_path_from_root: &str) {
 }
 
 pub fn create_file_if_not_exists(file_name: &str, relative_path: &str) {
-    let folder_path = env::current_dir().expect("Error getting current directory").join(relative_path);
+    let folder_path = get_current_path().join(relative_path);
     if !folder_path.exists() {
         fs::create_dir_all(&folder_path).expect("Failed to create folder.");
     }
@@ -51,14 +53,10 @@ pub fn create_file_if_not_exists(file_name: &str, relative_path: &str) {
     }
 }
 
-
-
-
-
 pub fn read_file_and_get_hash(file_path_from_root: &str) -> Result<(Vec<u8>, String), String> {
     // Open and read the file content as raw bytes
-    let mut file = File::open(file_path_from_root)
-        .map_err(|e| format!("Failed to open file: {}", e))?;
+    let mut file =
+        File::open(file_path_from_root).map_err(|e| format!("Failed to open file: {}", e))?;
     let mut content = Vec::new();
     file.read_to_end(&mut content)
         .map_err(|e| format!("Failed to read file: {}", e))?;
@@ -69,19 +67,25 @@ pub fn read_file_and_get_hash(file_path_from_root: &str) -> Result<(Vec<u8>, Str
     let hash_array = hasher.finalize();
 
     // Convert the hash to a hexadecimal string
-    let hash_string = hash_array.iter().map(|byte| format!("{:02x}", byte)).collect();
+    let hash_string = hash_array
+        .iter()
+        .map(|byte| format!("{:02x}", byte))
+        .collect();
 
     Ok((content, hash_string))
 }
 
-
 // Create blob file and save raw bytes
-pub fn create_blob_file_and_save(filename_from_root: String) -> String{
-    let (content, hashstring) = read_file_and_get_hash(&filename_from_root).expect("Failed to read file.");
+pub fn create_blob_file_and_save(filename_from_root: String) -> String {
+    let (content, hashstring) =
+        read_file_and_get_hash(&filename_from_root).expect("Failed to read file.");
     let hashname = &hashstring[0..6];
 
-    let blob_filename = format!("vcs/objects/{}/{}.blob", hashname, hashstring);
-    let foldername = format!("vcs/objects/{}", hashname);
+    let blob_filename = format!(
+        "version_control_system/objects/{}/{}.blob",
+        hashname, hashstring
+    );
+    let foldername = format!("version_control_system/objects/{}", hashname);
     create_folder_if_not_exists(&foldername); // Assuming `create_folder_if_not_exists` creates the folder
 
     let mut blob_file = File::create(&blob_filename).expect("Cannot create blob file");
@@ -91,43 +95,41 @@ pub fn create_blob_file_and_save(filename_from_root: String) -> String{
     hashstring
 }
 
-
-/// Helper function to read a `.vcs.ignore` file and return a Vec of strings to ignore.
-fn read_ignore_file(ignore_file_path: &str) -> Vec<String> {
-    let mut ignore_list = Vec::new();
+/// Helper function to read a `.version_control_system.ignore` file and return a Vec of strings to ignore.
+fn read_ignore_file(ignore_file_path: &str) -> HashSet<String> {
+    let mut ignore_list = HashSet::<String>::new();
     if let Ok(contents) = fs::read_to_string(ignore_file_path) {
         for line in contents.lines() {
             if !line.trim().is_empty() {
                 println!("Ignoring file: {}", line);
-                ignore_list.push(line.trim().to_string());
+                ignore_list.insert(line.trim().to_string());
             }
         }
     }
     ignore_list
 }
 
-/// Checks if the given path (file or folder) should be ignored based on `.vcs.ignore`.
-fn is_ignored(path: &Path, ignore_list: &Vec<String>, is_folder: bool) -> bool {
-    let file_name= path.file_name().unwrap().to_str().unwrap().to_string();
+/// Checks if the given path (file or folder) should be ignored based on `.version_control_system.ignore`.
+fn is_ignored(path: &Path, ignore_list: &HashSet<String>, is_folder: bool) -> bool {
+    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
     if is_folder {
-
-        return  ignore_list.contains(&format!("/{}", file_name).to_string())
-
+        return ignore_list.contains(&format!("/{}", file_name).to_string());
     }
 
     ignore_list.contains(&file_name)
 }
 /// Recursively create the folder hierarchy and add file hashes, considering ignored files and directories.
 pub fn create_hierarchy_of_folders(folder_path: &str) -> HashMap<String, Value> {
+    println!("####################### {:?}", folder_path);
     let mut folder_structure = HashMap::new();
 
     // Initialize the ignore list
-    let mut ignore_list = Vec::<String>::new();
+    let mut ignore_list =HashSet::<String>::new();
 
-    // Check if `.vcs.ignore` exists in the current folder
+    // Check if `.version_control_system.ignore` exists in the current folder
     let ignore_file_path = Path::new(folder_path).join(".vcs.ignore");
     if ignore_file_path.exists() {
-        // Load ignore list if `.vcs.ignore` file is found
+        // Load ignore list if `.version_control_system.ignore` file is found
         ignore_list = read_ignore_file(ignore_file_path.to_str().unwrap());
     }
 
@@ -138,8 +140,14 @@ pub fn create_hierarchy_of_folders(folder_path: &str) -> HashMap<String, Value> 
             if entry_path.is_dir() {
                 // If the directory is not in the ignore list, enqueue it for processing
                 if !is_ignored(&entry_path, &ignore_list, true) {
-                    let folder_name = entry_path.file_name().unwrap().to_str().unwrap().to_string();
-                    let subfolder_structure = create_hierarchy_of_folders(entry_path.to_str().unwrap());
+                    let folder_name = entry_path
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                    let subfolder_structure =
+                        create_hierarchy_of_folders(entry_path.to_str().unwrap());
 
                     // Convert HashMap to serde_json::Map
                     let map: Map<String, Value> = subfolder_structure.into_iter().collect();
@@ -147,7 +155,12 @@ pub fn create_hierarchy_of_folders(folder_path: &str) -> HashMap<String, Value> 
                 }
             } else {
                 // Process files and add their hash directly, if not ignored
-                let file_name = entry_path.file_name().unwrap().to_str().unwrap().to_string();
+                let file_name = entry_path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
                 if !is_ignored(&entry_path, &ignore_list, false) {
                     let hashstring = create_blob_file_and_save(entry_path.display().to_string());
                     folder_structure.insert(file_name, Value::String(hashstring));
@@ -164,8 +177,6 @@ pub fn save_hierarchy_to_file(folder_path: &str, output_file: &str) {
     let json_string = serde_json::to_string_pretty(&hierarchy).expect("Failed to serialize JSON");
     fs::write(output_file, json_string).expect("Failed to write JSON to file");
 }
-
-
 
 pub fn read_hierarchy_from_file(file_path: &str) -> Value {
     let file_content = fs::read_to_string(file_path).expect("Failed to read JSON file");
@@ -188,5 +199,117 @@ pub fn print_hierarchy(json_value: &Value, indent: usize) {
     }
 }
 
+pub fn get_current_dir_entities(current_path: PathBuf) -> HashSet<String>{
+    let mut entity_list = HashSet::<String>::new();
+    println!("####################### {:?}", current_path.display());
+    if let Ok(entries) = fs::read_dir(current_path) {
+        for entry in entries.filter_map(Result::ok) {
+            let entry_path = entry.path();
+            if entry_path.is_dir() {
+                let folder_name = entry_path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+                entity_list.insert(folder_name);
+            } else {
+                let file_name = entry_path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+                entity_list.insert(file_name);
+            }
+        }
+    }
+
+    entity_list
+}
+
+pub fn traverse_json(json: &Value, current_path: PathBuf) {
+    match json {
+        Value::Object(map) => {
+            // Check if the object is empty
+            if map.is_empty() {
+                println!("{}: {{}}", current_path.display()); // Print empty object
+            }
+
+            // Traverse each key-value pair in the object
+            for (key, value) in map {
+                let mut new_path = current_path.clone();
+                new_path.push(key); // Append the key to the current path
+                traverse_json(value, new_path); // Recurse with the updated path
+            }
+        }
+        Value::Array(arr) => {
+            for (index, value) in arr.iter().enumerate() {
+                let mut new_path = current_path.clone();
+                new_path.push(format!("[{}]", index)); // Append array index to the path
+                traverse_json(value, new_path); // Recurse with the updated path
+            }
+        }
+        _ => {
+            println!("{}: {}", current_path.display(), json); // Print the path and value
+        }
+    }
+}
 
 
+pub fn traverse_json3(json: &Value, current_path: PathBuf) {
+    let mut ignore_list = HashSet::<String>::new();
+    let mut entity_list = get_current_dir_entities(current_path.clone());
+
+    // Check if `.version_control_system.ignore` exists in the current folder and load the ignore list
+    let ignore_file_path = current_path.join(".vcs.ignore");
+    if ignore_file_path.exists() {
+        ignore_list = read_ignore_file(ignore_file_path.to_str().unwrap());
+
+        println!("{:?}", entity_list);
+        println!("{:?}", ignore_list);
+        println!("\n\n\n");
+    }
+
+    match json {
+        Value::Object(map) => {
+            // Traverse the object (folder) content
+            for (key, value) in map {
+                println!("key: {}", key);
+                if !ignore_list.contains(&key.to_string()) {
+
+                    let mut new_path = current_path.clone();
+                    new_path.push(key);
+                    println!("{} ------>", new_path.display());
+
+                    if entity_list.contains(&key.to_string()) {
+                        println!("key: {}", key);
+                    }
+                }
+            }
+        }
+        Value::Array(arr) => {
+            for (index, value) in arr.iter().enumerate() {
+                println!("value : {}", value);
+                let mut new_path = current_path.clone();
+                new_path.push(format!("[{}]", index)); // Append array index to the path
+
+                traverse_json3(value, new_path); // Recurse with the updated path
+            }
+        }
+        _ => {
+            if !is_ignored(&current_path, &ignore_list, false) {
+                println!("{}: {}", current_path.display(), json); // Print file path and content
+            }
+        }
+    }
+}
+
+pub fn main() {
+    let json_data = fs::read_to_string("version_control_system/config/struct.json")
+        .expect("Failed to read JSON");
+    let parsed_json: Value = serde_json::from_str(&*json_data).expect("Failed to parse JSON");
+    println!("\n\n");
+
+    traverse_json3(&parsed_json, get_current_path().join("test_fold"));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+use serde_json::Value;
 use crate::feature::{create_file_if_not_exists, create_folder_if_not_exists, print_hierarchy, read_hierarchy_from_file, save_hierarchy_to_file};
 mod feature;
 mod parse;
 
 fn main() {
-   init();
+   // traverse_json();
+    feature::main();
+    init();
 
 }
 
 
 fn init() {
-    create_folder_if_not_exists("vcs");
-    create_folder_if_not_exists("vcs/objects");
-    create_folder_if_not_exists("vcs/config");
-    create_file_if_not_exists("config.txt", "vcs/config");
-    create_file_if_not_exists("struct.json", "vcs/config");
-    save_hierarchy_to_file("test_fold","vcs/config/struct.json", );
+    create_folder_if_not_exists("version_control_system");
+    create_folder_if_not_exists("version_control_system/objects");
+    create_folder_if_not_exists("version_control_system/config");
+   create_file_if_not_exists("config.txt", "version_control_system/config");
+    create_file_if_not_exists("struct.json", "version_control_system/config");
+   save_hierarchy_to_file("test_fold","version_control_system/config/struct.json", );
  //
- //    let json_file_path = "vcs/config/struct.json";
+ //    let json_file_path = "version_control_system/config/struct.json";
  //    let hierarchy = read_hierarchy_from_file(json_file_path);
  //    println!("Folder Hierarchy:");
  // print_hierarchy(&hierarchy, 0);
 
 }
+
+
